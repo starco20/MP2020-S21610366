c264d3b753f2f30711b4e57fd2605ea1d1bc44ef
starco20 <63180549+starco20@users.noreply.github.com>
2020-06-08 16:49:39 +0800

    tugaz 2

---
+++ pertemuan3/VariableAndExpression/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Welcome To Multiplatform programming class");
+
+        int myfirstnumber = (10 + 5) + (5 * 2);
+        int mysecondnumber = 20;
+        int mythirdnumber = myfirstnumber * 5;
+        int total= myfirstnumber + mysecondnumber + mythirdnumber;
+
+        System.out.println(myfirstnumber);
+    }
+}
